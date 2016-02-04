4cbe3a598ea209f74e289ace855d23c3acb10434
Emil Edholm <bobby@verypublic.org>
2016-02-04 20:07:22 +0100

    Argument parsing

--- log-to-rss.py
+++ log-to-rss.py
@@ -1,2 +1,14 @@
-#!/usr/bin/env pyton
+#!/usr/bin/env python
 # Takes an announce log and outputs an RSS feed/file
+
+import argparse as ap
+
+if __name__ == '__main__':
+    parser = ap.ArgumentParser(description='Parse announce logs and output RSS file.')
+    parser.add_argument('logfiles', metavar='LOG', type=str, nargs='+',
+                        help='Input logfiles to parse')
+    parser.add_argument('-o', '--output', dest='output', metavar='PATH', default='announce.xml',
+                        help='where to store the finished RSS XML')
+
+    args = parser.parse_args()
+    print(args)
