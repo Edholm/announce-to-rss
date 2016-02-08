ba1f85f98ac02c07f557eb7794f5f39200f49573
Emil Edholm <bobby@verypublic.org>
2016-02-08 20:57:00 +0100

    Add cmd execution and better flag support

--- rss-downloader.sh
+++ rss-downloader.sh
@@ -1,16 +1,71 @@
 #!/usr/bin/bash
+# Script to download a file on a user supplied interval. If the new file
+# differs from the old, overwrite it, and run the supplied --cmd (if applicable)
 
-url="$1"
-interval="$2"
-dest="$3"
+while [[ $# > 1 ]]; do
+  key="$1"
 
-if [ -z "$url" ] || [ -z "$dest" ] || [ -z "$interval" ]; then
-    echo RSS feed URL, interval, and output destination need to be supplied
-    exit 1
+  case $key in
+    -u|--url)
+    URL="$2"
+    shift
+    ;;
+    -i|--interval)
+    INTERVAL="$2"
+    shift
+    ;;
+    -d|--dest)
+    DEST="$2"
+    shift
+    ;;
+    -c|--cmd)
+    CMD="$2"
+    shift
+    ;;
+    *)
+    echo "Unknown option: " "${key}"
+    ;;
+  esac
+  shift
+done
+
+die() {
+  echo "Error: $1"
+  exit 1
+}
+info() {
+  echo "Info: $1"
+}
+
+if [ -z "${INTERVAL}" ]; then
+  # 30 min default value
+  INTERVAL=1800
+  info "using default interval of ${INTERVAL}s"
 fi
 
-while true ; do
-    wget "$url" -O "$dest" -nv
-    touch "$dest"
-    sleep $interval
+if [ -z "${URL}" ] || [ -z "${DEST}" ]; then
+  die "--url and --dest is required"
+fi
+
+
+while true; do
+  tmpfile=$(mktemp)
+  if wget "${URL}" -O "${tmpfile}" -nv; then
+    if [ -f "${DEST}" ]; then
+      # Check if different
+      if ! cmp "${DEST}" "${tmpfile}" --silent; then
+        different=true
+      fi
+    fi
+
+    mv "${tmpfile}" "${DEST}"
+    touch "${DEST}"
+    if [ "$different" = true ]; then
+      info "New file is different, executing command"
+      eval ${CMD}
+    fi
+  else
+    info "wget failed..."
+  fi
+  sleep ${INTERVAL}
 done
